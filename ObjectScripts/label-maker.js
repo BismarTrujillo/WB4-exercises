e66115b8d6956c91d0382606dd601b0f408ee79f
Bismar Trujillo <bismar20@hotmail.com>
2022-10-31 14:31:13 -0400

    exercise 1 done

---
+++ ObjectScripts/label-maker.js
@@ -0,0 +1,14 @@
+"use strict";
+
+let person = {
+    name: "John Boyd",
+    adress: "2313 Ocean View Blvd",
+    city: "Palm Beach",
+    state: "FL",
+    zip: 33480,
+};
+
+function printContact(person){
+    console.log(person);
+}
+printContact(person);
