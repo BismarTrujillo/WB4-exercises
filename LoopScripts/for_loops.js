f5b7a835afacf3ba523da3a69d9bb44e487f6c45
Bismar Trujillo <bismar20@hotmail.com>
2022-11-01 09:07:39 -0400

    loop exercise

---
+++ LoopScripts/for_loops.js
@@ -0,0 +1,6 @@
+"use strict";
+
+for(let i =0;i<8;i++){
+    console.log(i+"I love loops");
+
+}
---
+++ LoopScripts/while_loops.js
@@ -0,0 +1,7 @@
+"use strict";
+let i = 0;
+
+while (i < 8) {
+    console.log(i + "I love loops");
+    i++;
+}
